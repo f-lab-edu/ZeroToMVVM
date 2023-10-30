b3b0b83030cd690c4f3e62bd1052c9e2837a8085
Tom <blak1002@gmail.com>
2023-10-30 23:59:37 +0900

    [feat(MainActivity.kt)] Fragment 화면 전환 트랜젝션 처리
    1. Fragment전환시 스크롤 유지를 위해서 show & hide 방식으로 구성

--- app/src/main/java/com/kova700/zerotomvvm/MainActivity.kt
+++ app/src/main/java/com/kova700/zerotomvvm/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.kova700.zerotomvvm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val bottomNavigationView by lazy { findViewById<BottomNavigationView>(R.id.bottomNavigationView) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initFragmentContainer(savedInstanceState)
+        initBottomNavigationView()
+    }
+
+    private fun initBottomNavigationView() {
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.bottom_menu_home -> showFragment(HOME_FRAGMENT_TAG)
+                R.id.bottom_menu_wish -> showFragment(WISH_FRAGMENT_TAG)
+            }
+            true
+        }
+    }
+
+    private fun initFragmentContainer(savedInstanceState: Bundle?) {
+        if (savedInstanceState != null) return
+
+        val homeFragment = HomeFragment()
+        val wishFragment = WishFragment()
+
+        with(supportFragmentManager) {
+            beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.container_main, wishFragment, WISH_FRAGMENT_TAG)
+                .hide(wishFragment)
+                .commit()
+
+            beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.container_main, homeFragment, HOME_FRAGMENT_TAG)
+                .commit()
+        }
+    }
+
+    private fun showFragment(fragmentTag: String) {
+        if (fragmentTag !in fragmentTagList) throw Exception() //올바른 예외로 수정
+
+        val targetFragment = getFragmentByTag(fragmentTag)
+        val transaction = supportFragmentManager.beginTransaction()
+        supportFragmentManager.fragments.forEach { fragmentInTransaction ->
+            if (fragmentInTransaction !== targetFragment) transaction.hide(fragmentInTransaction)
+        }
+        transaction.show(targetFragment)
+        transaction.commitNow()
+    }
+
+    private fun getFragmentByTag(tag: String) =
+        supportFragmentManager.findFragmentByTag(tag) ?: throw Exception() //올바른 예외로 수정
+
+    companion object {
+        const val HOME_FRAGMENT_TAG = "HOME_FRAGMENT_TAG"
+        const val WISH_FRAGMENT_TAG = "WISH_FRAGMENT_TAG"
+        val fragmentTagList = listOf(HOME_FRAGMENT_TAG, WISH_FRAGMENT_TAG)
     }
 }
