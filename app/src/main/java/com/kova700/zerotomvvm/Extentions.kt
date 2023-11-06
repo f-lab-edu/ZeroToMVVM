0a694b14f52180765bdecf9207c89bf2e31e3421
Tom <blak1002@gmail.com>
2023-11-07 05:01:55 +0900

    [refector(DetailActivity)] 선택된 아이템 데이터가 없을 시 예외 처리 추가
    1. 선택된 아이템 데이터가 없다면 유저에게 스낵바 노출

--- app/src/main/java/com/kova700/zerotomvvm/Extentions.kt
+++ app/src/main/java/com/kova700/zerotomvvm/Extentions.kt
@@ -3,7 +3,9 @@ package com.kova700.zerotomvvm
 import android.app.Activity
 import android.content.Intent
 import android.os.Build
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import java.io.Serializable
 
 fun <T : Serializable> Intent.getSerializableExtraData(key: String, clazz: Class<T>): T? =
@@ -25,4 +27,10 @@ fun <T : Serializable> Activity.finishWithSerializableExtra(key: String, extraDa
 fun Activity.finishWithExtra() {
     setResult(AppCompatActivity.RESULT_OK, intent)
     finish()
+}
+
+fun Activity.showSnackBar(layout: View, anchorView: View, textId: Int) {
+    Snackbar.make(layout, textId, Snackbar.LENGTH_SHORT)
+        .setAnchorView(anchorView)
+        .show()
 }
