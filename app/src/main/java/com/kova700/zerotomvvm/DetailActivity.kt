1f61ed4a89a525475f3715bcc3153224d69079bf
Tom <blak1002@gmail.com>
2023-11-07 00:07:52 +0900

    [style(DetailActivty)] viewBinding 적용

--- app/src/main/java/com/kova700/zerotomvvm/DetailActivity.kt
+++ app/src/main/java/com/kova700/zerotomvvm/DetailActivity.kt
@@ -1,32 +1,26 @@
 package com.kova700.zerotomvvm
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
-import android.widget.ToggleButton
 import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.kova700.zerotomvvm.MainActivity.Companion.TO_DETAIL_ITEM_POSITION_EXTRA
 import com.kova700.zerotomvvm.MainActivity.Companion.TO_DETAIL_SELECTED_ITEM_EXTRA
+import com.kova700.zerotomvvm.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity(R.layout.activity_detail) {
+    private lateinit var binding: ActivityDetailBinding
 
     private val pokemonListItem by lazy {
         intent.getSerializableExtraData(TO_DETAIL_SELECTED_ITEM_EXTRA, PokemonListItem::class.java)
             ?: throw Exception("pokemon is not exist")
     }
 
-    private val pokemonImageView: ImageView by lazy {
-        findViewById(R.id.iv_pokemon_image_detail_activity)
-    }
-
-    private val heartToggleButton: ToggleButton by lazy {
-        findViewById(R.id.tg_detail_activity)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setBackPressedEvent()
         initUIComponent()
     }
@@ -34,9 +28,9 @@ class DetailActivity : AppCompatActivity(R.layout.activity_detail) {
     private fun initUIComponent() {
         Glide.with(this)
             .load(pokemonListItem.pokemon.getImageUrl())
-            .into(pokemonImageView)
+            .into(binding.ivPokemonImageDetailActivity)
 
-        heartToggleButton.apply {
+        binding.tgDetailActivity.apply {
             isChecked = pokemonListItem.heart
             setOnClickListener {
                 pokemonListItem.heart = !pokemonListItem.heart
@@ -52,7 +46,10 @@ class DetailActivity : AppCompatActivity(R.layout.activity_detail) {
 
     private fun finishActivity() {
         intent.putExtra(TO_MAIN_HEART_BOOLEAN_EXTRA, pokemonListItem.heart)
-        intent.putExtra(TO_MAIN_ITEM_POSITION_EXTRA, intent.getIntExtraData(TO_DETAIL_ITEM_POSITION_EXTRA))
+        intent.putExtra(
+            TO_MAIN_ITEM_POSITION_EXTRA,
+            intent.getIntExtraData(TO_DETAIL_ITEM_POSITION_EXTRA)
+        )
         finishWithExtra()
     }
 
