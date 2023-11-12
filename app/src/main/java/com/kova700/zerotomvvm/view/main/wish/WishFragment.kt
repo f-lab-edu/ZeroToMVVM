834c5e70ab5aa859a26b2526cb6a0d5e1908075a
Tom <blak1002@gmail.com>
2023-11-12 14:03:51 +0900

    [refector(WishFragment)] WishPresenter로 Adapter 상호작용 코드 이전

--- app/src/main/java/com/kova700/zerotomvvm/view/main/wish/WishFragment.kt
+++ app/src/main/java/com/kova700/zerotomvvm/view/main/wish/WishFragment.kt
@@ -2,7 +2,6 @@ package com.kova700.zerotomvvm.view.main.wish
 
 import android.app.Activity
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
 import android.view.LayoutInflater
@@ -11,29 +10,32 @@ import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.kova700.zerotomvvm.R
-import com.kova700.zerotomvvm.data.source.pokemon.PokemonListItem
 import com.kova700.zerotomvvm.databinding.FragmentWishBinding
 import com.kova700.zerotomvvm.util.getBooleanExtraData
 import com.kova700.zerotomvvm.util.getIntExtraData
-import com.kova700.zerotomvvm.view.detail.DetailActivity
 import com.kova700.zerotomvvm.view.detail.DetailActivity.Companion.TO_MAIN_HEART_BOOLEAN_EXTRA
 import com.kova700.zerotomvvm.view.detail.DetailActivity.Companion.TO_MAIN_ITEM_POSITION_EXTRA
 import com.kova700.zerotomvvm.view.main.MainActivity
-import com.kova700.zerotomvvm.view.main.MainActivity.Companion.TO_DETAIL_ITEM_POSITION_EXTRA
-import com.kova700.zerotomvvm.view.main.MainActivity.Companion.TO_DETAIL_SELECTED_ITEM_EXTRA
-import com.kova700.zerotomvvm.view.main.adapter.PokemonItemClickListener
 import com.kova700.zerotomvvm.view.main.adapter.PokemonListAdapter
-import kotlinx.coroutines.launch
+import com.kova700.zerotomvvm.view.main.wish.presenter.WishContract
+import com.kova700.zerotomvvm.view.main.wish.presenter.WishPresenter
 
-class WishFragment : Fragment() {
+class WishFragment : Fragment(), WishContract.View {
 
     private var _binding: FragmentWishBinding? = null
     private val binding get() = _binding!!
-    lateinit var wishAdapter: PokemonListAdapter
+    private val wishAdapter by lazy { PokemonListAdapter() }
     private lateinit var mainActivity: MainActivity
+
+    private val presenter by lazy {
+        WishPresenter(
+            view = this@WishFragment,
+            adapterView = wishAdapter,
+            adapterModel = wishAdapter,
+        )
+    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -51,27 +53,11 @@ class WishFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initAdapter()
         initRecyclerView()
-        observeWishPokemonData()
+        presenter.observeWishPokemonList()
     }
 
-    private fun observeWishPokemonData() = lifecycleScope.launch {
-        mainActivity.presenter.observeWishPokemonList {
-            wishAdapter.submitList(it)
-        }
-    }
-
-    //어디까지 Presenter로 가져가고 어디까지는 Activity에 남기는 것을 허용하는가?
-    //  1. 새로운 Activity 호출은?
-    //  2. registerForActivityResult도 사실 생명주기만 잘타면 옮길 수 있지 않나?
-    //  3. observe 하고 있는거도 옮기나?
-    //  4. RecyclerView Item이 아닌 바로 해당 View에서 의 onClick도 Presenter로 옮겨서 정의하나?
-    //Activity가 오로지 View의 역할만 한다는 것이 정확이 어느 의미일까 정확하게 파악하고 분리하자.
-    //Controller의 역할이 뭐였나? (이벤트를 생성 -> Model에 업데이트 요청 -> View에게 전달 -> View 갱신)
-    //MVC에서는 View에서 Controller의 이벤트를 통지받고, Model도 함께 사용한다.
-    //DataBinding을 안쓰면 setOnClickListener다 연결해줘야하나?
-    private val activityResultLauncher =
+    val activityResultLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode != Activity.RESULT_OK) return@registerForActivityResult
 
@@ -79,27 +65,8 @@ class WishFragment : Fragment() {
             val heartValue = intent.getBooleanExtraData(TO_MAIN_HEART_BOOLEAN_EXTRA)
             val itemPosition = intent.getIntExtraData(TO_MAIN_ITEM_POSITION_EXTRA)
             if (heartValue) return@registerForActivityResult
-            removeWishItem(wishAdapter.currentList[itemPosition])
+            presenter.deletePokemonInWishItem(wishAdapter.currentList[itemPosition])
         }
-
-    private fun initAdapter() {
-        val itemClickListener = object : PokemonItemClickListener {
-            override fun onItemClick(itemPosition: Int) {
-                val selectedItem = wishAdapter.currentList[itemPosition]
-                val intent = Intent(requireActivity(), DetailActivity::class.java).apply {
-                    putExtra(TO_DETAIL_SELECTED_ITEM_EXTRA, selectedItem)
-                    putExtra(TO_DETAIL_ITEM_POSITION_EXTRA, itemPosition)
-                }
-                activityResultLauncher.launch(intent)
-            }
-
-            override fun onHeartClick(itemPosition: Int) {
-                val selectedItem = wishAdapter.currentList[itemPosition]
-                if (selectedItem.heart) removeWishItem(selectedItem)
-            }
-        }
-        wishAdapter = PokemonListAdapter(itemClickListener)
-    }
 
     private fun initRecyclerView() {
         binding.rcvWishFragment.apply {
@@ -117,10 +84,6 @@ class WishFragment : Fragment() {
                     onRestoreInstanceState(arguments?.getParcelable(WISH_RCV_STATE_KEY))
                 }
         }
-    }
-
-    private fun removeWishItem(selectedItem: PokemonListItem) {
-        mainActivity.presenter.deletePokemonItem(selectedItem)
     }
 
     override fun onDestroyView() {
