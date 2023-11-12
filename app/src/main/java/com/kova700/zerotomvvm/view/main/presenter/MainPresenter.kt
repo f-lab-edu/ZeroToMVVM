97e04baf3eac403a6015c17b21502bff17015af2
Tom <blak1002@gmail.com>
2023-11-12 13:26:47 +0900

    [style(MainPresenter)] repositoy 변수명 수정

--- app/src/main/java/com/kova700/zerotomvvm/view/main/presenter/MainPresenter.kt
+++ app/src/main/java/com/kova700/zerotomvvm/view/main/presenter/MainPresenter.kt
@@ -10,48 +10,44 @@ import kotlinx.coroutines.launch
 
 class MainPresenter(
     private val view: MainContract.View,
-    private val mainRepository: PokemonRepository
-    //어댑터를 또 Model 형식과 View형식으로 찢어야함
-    //근데 어댑터가 HomeFragment에서 쓰는 게 있고, WishFragment에서 쓰는게 있는데
-    //여기에 전부 가져와야할까?
-    //아니면 각 Fragment별로 Presenter를 따로 만든다던가
+    private val repository: PokemonRepository
 ) : MainContract.Presenter {
 
     private val activity = view as MainActivity
 
     override fun loadPokemonList() {
         activity.lifecycleScope.launch {
-            runCatching { mainRepository.loadPokemonList() }
+            runCatching { repository.loadPokemonList() }
                 .onFailure { activity.showToast("data load를 실패했습니다. : ${it.message}") }
         }
     }
 
     override fun updatePokemonList(newList: List<PokemonListItem>) {
         activity.lifecycleScope.launch {
-            mainRepository.pokemonListFlow.emit(newList)
+            repository.pokemonListFlow.emit(newList)
         }
     }
 
     override fun deletePokemonItem(selectedItem: PokemonListItem) {
         activity.lifecycleScope.launch {
-            val currentList = mainRepository.pokemonListFlow.first().toMutableList()
+            val currentList = repository.pokemonListFlow.first().toMutableList()
             currentList.forEachIndexed { index, pokemonListItem ->
                 if (pokemonListItem.pokemon.name != selectedItem.pokemon.name) return@forEachIndexed
                 currentList[index] = selectedItem.copy(heart = false)
             }
-            mainRepository.pokemonListFlow.emit(currentList)
+            repository.pokemonListFlow.emit(currentList)
         }
     }
 
     override fun observePokemonList(callback: (List<PokemonListItem>) -> Unit) {
         activity.lifecycleScope.launch {
-            mainRepository.pokemonListFlow.collect { callback(it) }
+            repository.pokemonListFlow.collect { callback(it) }
         }
     }
 
     override fun observeWishPokemonList(callback: (List<PokemonListItem>) -> Unit) {
         activity.lifecycleScope.launch {
-            mainRepository.wishPokemonListFlow.collect { callback(it) }
+            repository.wishPokemonListFlow.collect { callback(it) }
         }
     }
 
