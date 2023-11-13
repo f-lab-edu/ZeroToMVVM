5d80835b0f68d1566c73904b047028edbf978549
Tom <blak1002@gmail.com>
2023-11-13 12:46:09 +0900

    [config(app:build.gradle)] KSP 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     kotlin("plugin.serialization") version "1.9.0"
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14"
 }
 
 android {
@@ -49,6 +50,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //KSP
+    implementation("com.google.devtools.ksp:symbol-processing-api:1.9.20-1.0.14")
     //Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
     //Retrofit
