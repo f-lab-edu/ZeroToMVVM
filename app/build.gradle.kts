827b4b5eb393f6c2ac976b14f03996fe4bf21108
Tom <blak1002@gmail.com>
2023-11-13 12:47:09 +0900

    [config(app:build.gradle)] Room 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,9 @@ dependencies {
     val nav_version = "2.7.5"
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+    //Room
+    val room_version = "2.6.0"
+    implementation("androidx.room:room-runtime:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
 }
