44d955743dbe1621a32b6c189616fa4b892e0efa
Tom <blak1002@gmail.com>
2023-11-15 17:10:44 +0900

    [fix(DetailActivity)] Activity 생성 인자에 불필요한 layout xml 전달 제거

--- app/src/main/java/com/kova700/zerotomvvm/view/detail/DetailActivity.kt
+++ app/src/main/java/com/kova700/zerotomvvm/view/detail/DetailActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.kova700.zerotomvvm.R
 import com.kova700.zerotomvvm.data.source.pokemon.PokemonListItem
 import com.kova700.zerotomvvm.databinding.ActivityDetailBinding
 import com.kova700.zerotomvvm.util.getIntExtraData
@@ -13,7 +12,7 @@ import com.kova700.zerotomvvm.util.getSerializableExtraData
 import com.kova700.zerotomvvm.view.main.MainActivity.Companion.TO_DETAIL_ITEM_POSITION_EXTRA
 import com.kova700.zerotomvvm.view.main.MainActivity.Companion.TO_DETAIL_SELECTED_ITEM_EXTRA
 
-class DetailActivity : AppCompatActivity(R.layout.activity_detail) {
+class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
     private lateinit var pokemonListItem: PokemonListItem
 
