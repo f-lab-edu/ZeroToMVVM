c76d7e12a41f3aca74ed69bae9f74548c1a98187
Tom <blak1002@gmail.com>
2023-11-16 23:17:08 +0900

    [fix(PokemonListItem)] fix typo

--- app/src/main/java/com/kova700/zerotomvvm/data/source/pokemon/PokemonListItem.kt
+++ app/src/main/java/com/kova700/zerotomvvm/data/source/pokemon/PokemonListItem.kt
@@ -8,10 +8,10 @@ data class Pokemon(
     @SerialName("name") val name: String,
     @SerialName("url") val detailInfoUrl: String
 ) : Serializable {
-    fun pokemonNum() = detailInfoUrl.split("/".toRegex()).dropLast(1).last().toInt()
+    fun getPokemonNum() = detailInfoUrl.split("/".toRegex()).dropLast(1).last().toInt()
     fun getImageUrl(): String {
         return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/" +
-                "pokemon/other/official-artwork/${pokemonNum()}.png"
+                "pokemon/other/official-artwork/${getPokemonNum()}.png"
     }
 }
 
