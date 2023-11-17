a4d82c01a169aae2bfbdd434bc0728e216989928
Tom <blak1002@gmail.com>
2023-11-17 23:25:19 +0900

    [feat(App.kt)] Application 클래스 추가

---
+++ app/src/main/java/com/kova700/zerotomvvm/App.kt
@@ -0,0 +1,16 @@
+package com.kova700.zerotomvvm
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    companion object {
+        lateinit var instance: App
+            private set
+    }
+}
