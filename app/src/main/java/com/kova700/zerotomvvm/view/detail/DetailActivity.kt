32776280bafa8e7e7fa426714c2d09f8c558c92b
Tom <blak1002@gmail.com>
2023-11-25 20:34:09 +0900

    [fix(Detail)] 하트 수정 결과 불일치 이슈 해결

--- app/src/main/java/com/kova700/zerotomvvm/view/detail/DetailActivity.kt
+++ app/src/main/java/com/kova700/zerotomvvm/view/detail/DetailActivity.kt
@@ -4,28 +4,35 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.kova700.zerotomvvm.data.api.PokemonApi
+import com.kova700.zerotomvvm.data.db.AppDataBase
 import com.kova700.zerotomvvm.data.source.pokemon.PokemonListItem
+import com.kova700.zerotomvvm.data.source.pokemon.remote.PokemonRepositoryImpl
 import com.kova700.zerotomvvm.databinding.ActivityDetailBinding
 import com.kova700.zerotomvvm.util.getSerializableExtraData
 import com.kova700.zerotomvvm.view.main.MainActivity.Companion.TO_DETAIL_SELECTED_ITEM_EXTRA
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
-    private lateinit var pokemonListItem: PokemonListItem
-    private val pokemonViewModel by viewModels<DetailViewModel> {
-        DetailViewModel.Factory
+    private val detailViewModel by viewModels<DetailViewModel> {
+        DetailViewModel.provideFactory(
+            PokemonRepositoryImpl.getInstance(
+                PokemonApi.service,
+                AppDataBase.service
+            ),
+            getPokemonExtraData()
+        )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        getPokemonExtraData()
         initUIComponent()
     }
 
-    private fun getPokemonExtraData() {
-        pokemonListItem = intent.getSerializableExtraData(
+    private fun getPokemonExtraData(): PokemonListItem {
+        return intent.getSerializableExtraData(
             TO_DETAIL_SELECTED_ITEM_EXTRA,
             PokemonListItem::class.java
         )!!
@@ -33,12 +40,12 @@ class DetailActivity : AppCompatActivity() {
 
     private fun initUIComponent() {
         Glide.with(this)
-            .load(pokemonListItem.pokemon.getImageUrl())
+            .load(detailViewModel.selectedItem.pokemon.getImageUrl())
             .into(binding.ivPokemonImageDetailActivity)
 
         binding.tgDetailActivity.apply {
-            isChecked = pokemonListItem.heart
-            setOnClickListener { pokemonViewModel.heartClickListener(pokemonListItem) }
+            isChecked = detailViewModel.selectedItem.heart
+            setOnClickListener { detailViewModel.heartClickListener() }
         }
     }
 }
--- app/src/main/java/com/kova700/zerotomvvm/view/detail/DetailViewModel.kt
+++ app/src/main/java/com/kova700/zerotomvvm/view/detail/DetailViewModel.kt
@@ -3,35 +3,33 @@ package com.kova700.zerotomvvm.view.detail
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.kova700.zerotomvvm.data.api.PokemonApi
-import com.kova700.zerotomvvm.data.db.AppDataBase
 import com.kova700.zerotomvvm.data.source.pokemon.PokemonListItem
 import com.kova700.zerotomvvm.data.source.pokemon.remote.PokemonRepository
-import com.kova700.zerotomvvm.data.source.pokemon.remote.PokemonRepositoryImpl
-import com.kova700.zerotomvvm.view.main.PokemonViewModel
 import kotlinx.coroutines.launch
 
-class DetailViewModel(private val pokemonRepository: PokemonRepository) : ViewModel() {
+class DetailViewModel(
+    private val pokemonRepository: PokemonRepository,
+    var selectedItem: PokemonListItem
+) : ViewModel() {
 
     private suspend fun updatePokemonHeart(targetPokemonNum: Int, heartValue: Boolean) {
         pokemonRepository.updatePokemonHeart(targetPokemonNum, heartValue)
     }
 
-    fun heartClickListener(selectedItem: PokemonListItem) = viewModelScope.launch {
-        updatePokemonHeart(selectedItem.pokemon.getPokemonNum(), selectedItem.heart.not())
+    fun heartClickListener() = viewModelScope.launch {
+        selectedItem = selectedItem.copy(heart = selectedItem.heart.not())
+        updatePokemonHeart(selectedItem.pokemon.getPokemonNum(), selectedItem.heart)
     }
 
     companion object {
-        val Factory = object : ViewModelProvider.Factory {
+        fun provideFactory(
+            pokemonRepository: PokemonRepository,
+            selectedItem: PokemonListItem
+        ) = object : ViewModelProvider.Factory {
 
             @Suppress("UNCHECKED_CAST")
             override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                return DetailViewModel(
-                    PokemonRepositoryImpl.getInstance(
-                        PokemonApi.service,
-                        AppDataBase.service
-                    )
-                ) as T
+                return DetailViewModel(pokemonRepository, selectedItem) as T
             }
         }
     }
