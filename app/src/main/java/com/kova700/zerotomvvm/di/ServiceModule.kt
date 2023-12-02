1765f3bf4b38c297073f0dac053d477e20694cf3
Tom <blak1002@gmail.com>
2023-12-02 21:12:46 +0900

    [feat] ServiceModule  추가

---
+++ app/src/main/java/com/kova700/zerotomvvm/di/ServiceModule.kt
@@ -0,0 +1,19 @@
+package com.kova700.zerotomvvm.di
+
+import com.kova700.zerotomvvm.data.api.PokemonService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+
+    @Provides
+    @Singleton
+    fun providePokemonService(retrofit: Retrofit): PokemonService =
+        retrofit.create(PokemonService::class.java)
+}
