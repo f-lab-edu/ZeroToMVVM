332ac920a556dd2ebb061317616742e99af30be7
Tom <blak1002@gmail.com>
2023-12-02 10:15:59 +0900

    [feat(App)] HiltAndroidApp 어노테이션 추가

--- app/src/main/java/com/kova700/zerotomvvm/App.kt
+++ app/src/main/java/com/kova700/zerotomvvm/App.kt
@@ -1,7 +1,9 @@
 package com.kova700.zerotomvvm
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application() {
 
     override fun onCreate() {
